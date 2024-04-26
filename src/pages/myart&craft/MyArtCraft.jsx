e85d97eeb1a33c9dcebc564b6a05b836958b0da1
Mehedi Hassan Miraj <dreammehedihassan@gmail.com>
2024-04-26 17:25:15 +0600

    My Art & Craft Ui Design

--- src/pages/myart&craft/MyArtCraft.jsx
+++ src/pages/myart&craft/MyArtCraft.jsx
@@ -1,5 +1,101 @@
+import { useLocation } from "react-router-dom";
+import Button from "../../components/button/Button";
+import SlugBanner from "../../components/slug_banner/SlugBanner";
 function MyArtCraft() {
-  return <div>MyArtCraft</div>;
+  // find path
+  const { pathname } = useLocation();
+  return (
+    <>
+      <SlugBanner path={pathname}></SlugBanner>
+
+      {/* my art & craft */}
+      <div className="my-4 md:my-6 lg:my-8 grid grid-cols-4 justify-between gap-4 md:gap-6 ">
+        <div className="relative m-10 w-full max-w-xs overflow-hidden rounded-lg bg-white shadow-md">
+          <a href="#">
+            <img
+              className="h-60 rounded-t-lg object-cover"
+              src="https://images.unsplash.com/flagged/photo-1556637640-2c80d3201be8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8c25lYWtlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+              alt="product image"
+            />
+          </a>
+          <span className="absolute top-0 left-0 w-40 translate-y-8 -translate-x-8 -rotate-45 bg-orange-900/50 text-center text-sm text-white">
+            In Stock
+          </span>
+          <div className="mt-4 px-5 pb-5 space-y-4">
+            <h5 className="text-xl font-semibold tracking-tight text-orange-900">
+              Nike Air MX Super 5000
+            </h5>
+
+            <div className="mt-2.5 mb-5 flex items-center">
+              <span className="mr-2 rounded bg-orange-500 px-2.5 py-0.5 text-xs font-semibold text-white">
+                5.0
+              </span>
+              <svg
+                aria-hidden="true"
+                className="h-5 w-5 text-orange-500"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+              </svg>
+              <svg
+                aria-hidden="true"
+                className="h-5 w-5 text-orange-500"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+              </svg>
+              <svg
+                aria-hidden="true"
+                className="h-5 w-5 text-orange-500"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+              </svg>
+              <svg
+                aria-hidden="true"
+                className="h-5 w-5 text-orange-500"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+              </svg>
+              <svg
+                aria-hidden="true"
+                className="h-5 w-5 text-orange-500"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+              </svg>
+            </div>
+            <div>
+              <p>
+                <span className="text-3xl font-bold text-slate-900">$249</span>
+              </p>
+            </div>
+            <p className="text-orange-900 font-bold">
+              Customization:{" "}
+              <span className="text-white rounded-md p-1 px-2 bg-orange-900 cursor-pointer font-medium">
+                Yes
+              </span>
+            </p>
+            <div className="flex justify-between items-center gap-3">
+              <Button name="Update" cls={"!text-white"}></Button>
+              <Button name="Delete" cls={"!text-white"}></Button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default MyArtCraft;
