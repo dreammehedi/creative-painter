344ca75e3c70072a5f6566de3fcb2c4db9b6c0c4
Mehedi Hassan Miraj <dreammehedihassan@gmail.com>
2024-04-27 12:37:40 +0600

    All art craft detailes page slugbanner just added

--- src/pages/art&craft/AllArtCraftDetailes.jsx
+++ src/pages/art&craft/AllArtCraftDetailes.jsx
@@ -1,10 +1,19 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useLocation } from "react-router-dom";
+import SlugBanner from "../../components/slug_banner/SlugBanner";
 
 function AllArtCraftDetailes() {
+  // find path
+  const { pathname } = useLocation();
+
   // art craft detailes data get
   const artCraftDetailesData = useLoaderData();
   console.log(artCraftDetailesData);
-  return <div>AllArtCraftDetailes</div>;
+
+  return (
+    <>
+      <SlugBanner path={pathname}></SlugBanner>
+    </>
+  );
 }
 
 export default AllArtCraftDetailes;
