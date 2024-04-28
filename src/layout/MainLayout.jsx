5bd449f376b1d2af406912d27997880c03b6e73f
Mehedi Hassan Miraj <dreammehedihassan@gmail.com>
2024-04-28 13:08:28 +0600

    Click to Top & Page Refresh Page Top

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,9 +1,17 @@
-import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { Outlet, useLocation } from "react-router-dom";
 import { Bounce, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import ClickToTop from "../components/clicktop/ClickToTop";
 import Footer from "../components/footer/Footer";
 import Navbar from "../components/header/Navbar";
 function MainLayout() {
+  // after location change page is scroll top
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }, [pathname]);
   return (
     <>
       <ToastContainer
@@ -16,6 +24,7 @@ function MainLayout() {
       <Navbar></Navbar>
       <Outlet></Outlet>
       <Footer></Footer>
+      <ClickToTop></ClickToTop>
     </>
   );
 }
