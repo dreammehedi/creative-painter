6234a5ac42cea41a614d0b0fd2f0764e7d015170
Mehedi Hassan Miraj <dreammehedihassan@gmail.com>
2024-04-28 18:13:15 +0600

    Section Title Responsive

--- src/components/section/SectionTitle.jsx
+++ src/components/section/SectionTitle.jsx
@@ -1,8 +1,8 @@
 import PropTypes from "prop-types";
 function SectionTitle({ title }) {
   return (
-    <div className="text-center">
-      <h2 className="text-center text-3xl md:text-4xl lg:text-5xl font-extrabold font-dancing-script text-orange-500">
+    <div className="text-center my-[40px] md:my-[80px]">
+      <h2 className="text-center text-3xl md:text-4xl lg:text-5xl font-extrabold font-poppins text-orange-500">
         {title}
       </h2>
     </div>
