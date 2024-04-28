fb602a3e86152060ac58d5c0cd65a1692933dc1b
Mehedi Hassan Miraj <dreammehedihassan@gmail.com>
2024-04-28 18:49:08 +0600

    Footer Responsive

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
   return (
     <footer className="px-4 divide-y bg-gray-100 text-gray-800">
       <div className="container flex flex-col justify-between py-10 mx-auto space-y-8 lg:flex-row lg:space-y-0">
-        <div className="lg:w-1/3">
+        <div className="lg:w-3/4 text-center lg:text-left mr-0 md:mr-4 lg:mr-6">
           <Logo mainCls={"text-orange-500"}></Logo>
           <Link to={"/"} className="flex flex-col space-y-3 ">
             <span className="text-2xl font-semibold text-orange-500">
@@ -21,8 +21,8 @@ function Footer() {
             friend, we're here to turn your visions into reality.`}
           </p>
         </div>
-        <div className="grid grid-cols-2 text-sm gap-x-3 gap-y-8 lg:w-2/3 sm:grid-cols-3">
-          <div className="space-y-3">
+        <div className="grid grid-cols-1 lg:grid-cols-2 items-center lg:items-start lg:text-left text-center gap-8 ">
+          <div className=" space-y-3">
             <h3 className="uppercase text-orange-500 font-medium">Shop</h3>
             <ul className="space-y-1">
               <li>
@@ -62,7 +62,7 @@ function Footer() {
                 Stay connected with the world of art and creativity by
                 subscribing to the Palette Portrait & Pencil newsletter.
               </p>
-              <form className="">
+              <form className="md:max-w-md md:mx-auto">
                 <div className="relative">
                   <input
                     className="px-4 py-2 outline-none ring-1 ring-orange-900 focus:ring-orange-500 rounded-md bg-transparent w-full"
@@ -80,31 +80,34 @@ function Footer() {
                 </div>
               </form>
             </div>
-            <div className="space-y-3">
-              <h3 className="uppercase text-orange-500 font-medium">
-                Contact Us
-              </h3>
-              <address>
-                <div className="space-y-2">
-                  <p className="font-bold flex items-center font-poppins text-orange-900">
-                    Email:{" "}
-                    <span className="font-medium ml-1">
-                      dreammehedihassan@gmail.com
-                    </span>
-                  </p>
-                  <p className="font-bold flex items-center font-poppins text-orange-900">
-                    Phone:{" "}
-                    <span className="font-medium ml-1">(+880) 1783238465</span>
-                  </p>
-                </div>
-              </address>
-            </div>
+          </div>
+          <div className="space-y-3">
+            <h3 className="uppercase text-orange-500 font-medium">
+              Contact Us
+            </h3>
+            <address>
+              <div className="space-y-2 flex flex-col justify-center items-center lg:items-start lg:text-left text-center">
+                <p
+                  className="font-bold text-center
+                   flex items-center font-poppins text-orange-900"
+                >
+                  Email:{" "}
+                  <span className="font-medium ml-1 ">
+                    dreammehedihassan@gmail.com
+                  </span>
+                </p>
+                <p className="font-bold flex items-center font-poppins text-orange-900">
+                  Phone:{" "}
+                  <span className="font-medium ml-1">(+880) 1783238465</span>
+                </p>
+              </div>
+            </address>
           </div>
           <div className="space-y-3">
             <div className="uppercase text-orange-500 font-medium">
               Social media
             </div>
-            <div className="flex justify-start space-x-3">
+            <div className="flex justify-center lg:justify-start space-x-3">
               <a
                 href="https://www.facebook.com/profile.php?id=61554869056271"
                 title="Facebook"
