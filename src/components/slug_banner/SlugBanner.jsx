77f470f8da37712aac7e0d311865d0ac5f94d3c9
Mehedi Hassan Miraj <dreammehedihassan@gmail.com>
2024-04-30 15:15:43 +0600

    Slug Banner Responsive

--- src/components/slug_banner/SlugBanner.jsx
+++ src/components/slug_banner/SlugBanner.jsx
@@ -1,10 +1,10 @@
 import PropTypes from "prop-types";
 function SlugBanner({ path }) {
   return (
-    <section className="bg-orange-50">
-      <div className="container flex justify-start items-center w-full h-[200px]  text-orange-900 font-bold font-poppins text-2xl md:text-3xl break-words text-wrap">
+    <section className="bg-orange-50 w-full h-[200px] flex items-center">
+      <h1 className="container lg:text-center text-orange-900 font-bold font-poppins text-2xl md:text-3xl break-words text-wrap overflow-hidden">
         {`Home ${path}`}
-      </div>
+      </h1>
     </section>
   );
 }
