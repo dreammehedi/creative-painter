0c92e03ee67968d491167435d51024be42126062
Mehedi Hassan Miraj <dreammehedihassan@gmail.com>
2024-04-30 13:32:57 +0600

    Update

--- src/components/art_craft_category/ArtCraftCategoryCart.jsx
+++ src/components/art_craft_category/ArtCraftCategoryCart.jsx
@@ -2,11 +2,13 @@ import { Link } from "react-router-dom";
 import errorImage from "../../assets/error-img.jpg";
 // import tooltip styles
 import PropTypes from "prop-types";
+import { Tooltip } from "react-tooltip";
 import "react-tooltip/dist/react-tooltip.css";
 
 function ArtCraftCategoryCart({ artCraftData, index }) {
-  const { image, main_category } = artCraftData;
-  // subcategory_name, item_name, price, rating
+  const { image, main_category, subcategory_name, item_name, price, rating } =
+    artCraftData;
+
   return (
     <Link to={`/all-art-craft-category/${main_category}`}>
       <div
@@ -21,24 +23,24 @@ function ArtCraftCategoryCart({ artCraftData, index }) {
         <h2 className="font-poppins text-2xl font-bold group-hover:text-orange-500 transition-all duration-300 ease-linear">
           {main_category}
         </h2>
+        <Tooltip id={`tooltip${index}`}>
+          <div className="py-4">
+            <p className="text-orange-500 text-xl font-semibold">
+              Title: <span className="text-white text-[18px]">{item_name}</span>
+            </p>
+            <p className="text-orange-500 text-xl font-semibold">
+              SubCategory:{" "}
+              <span className="text-white text-[18px]">{subcategory_name}</span>
+            </p>
+            <p className="text-orange-500 text-xl font-semibold">
+              Rating: <span className="text-white text-[18px]">{rating}</span>
+            </p>
+            <p className="text-orange-500 text-xl font-semibold">
+              Price: <span className="text-white text-[18px]">{price}</span>
+            </p>
+          </div>
+        </Tooltip>
       </div>
-      {/* <Tooltip id={`tooltip${index}`}>
-        <div className="py-4">
-          <p className="text-orange-500 text-xl font-semibold">
-            Title: <span className="text-white text-[18px]">{item_name}</span>
-          </p>
-          <p className="text-orange-500 text-xl font-semibold">
-            SubCategory:{" "}
-            <span className="text-white text-[18px]">{subcategory_name}</span>
-          </p>
-          <p className="text-orange-500 text-xl font-semibold">
-            Rating: <span className="text-white text-[18px]">{rating}</span>
-          </p>
-          <p className="text-orange-500 text-xl font-semibold">
-            Price: <span className="text-white text-[18px]">{price}</span>
-          </p>
-        </div>
-      </Tooltip> */}
     </Link>
   );
 }
